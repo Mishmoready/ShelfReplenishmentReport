edc46d5dc305b1a72ab187d02f88bfa740f1c448
Ben John <mozfly@gmail.com>
2023-11-28 23:09:13 +0000

    Cleaning out data

--- project1/webapp/controller/InitPage.js
+++ project1/webapp/controller/InitPage.js
@@ -18,29 +18,29 @@ sap.ui.define([
             const libInfo = await VersionInfo.load({
                 library: "sap.suite.ui.commons"
             });
-            if (libInfo != null) {
-                await Lib.load("sap.suite.ui.commons");
-                return new Promise((resolve, reject) => {
-                    sap.ui.require([
-                        "sap/suite/ui/commons/ChartContainer",
-                        "sap/suite/ui/commons/ChartContainerContent"
-                    ], (ChartContainer, ChartContainerContent) => {
-                        const vizframe = oView.byId("idVizFrame");
-                        const oChartContainerContent = new ChartContainerContent({
-                            icon : "sap-icon://pie-chart",
-                            title : "vizFrame Pie Chart Sample",
-                            content : [ vizframe ]
-                        });
-                        const oChartContainer = new ChartContainer({
-                            content : [ oChartContainerContent ]
-                        });
-                        oChartContainer.setShowFullScreen(true);
-                        oChartContainer.setAutoAdjustHeight(true);
-                        oView.byId("chartFixFlex").setFlexContent(oChartContainer);
-                        resolve(oView);
-                    }, reject);
-                });
-            }
+            // if (libInfo != null) {
+            //     await Lib.load("sap.suite.ui.commons");
+            //     return new Promise((resolve, reject) => {
+            //         sap.ui.require([
+            //             "sap/suite/ui/commons/ChartContainer",
+            //             "sap/suite/ui/commons/ChartContainerContent"
+            //         ], (ChartContainer, ChartContainerContent) => {
+            //             const vizframe = oView.byId("idVizFrame");
+            //             const oChartContainerContent = new ChartContainerContent({
+            //                 icon : "sap-icon://pie-chart",
+            //                 title : "vizFrame Pie Chart Sample",
+            //                 content : [ vizframe ]
+            //             });
+            //             const oChartContainer = new ChartContainer({
+            //                 content : [ oChartContainerContent ]
+            //             });
+            //             oChartContainer.setShowFullScreen(true);
+            //             oChartContainer.setAutoAdjustHeight(true);
+            //             oView.byId("chartFixFlex").setFlexContent(oChartContainer);
+            //             resolve(oView);
+            //         }, reject);
+            //     });
+            // }
         }
     };
 });
