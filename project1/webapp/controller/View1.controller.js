505f2370518a9d542a253e61f5cdbc7d6d37d0ad
Ben John <mozfly@gmail.com>
2023-12-02 01:41:42 +0000

    New additions

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,15 +1,19 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
     function (Controller) {
-        "use strict";
-
-        return Controller.extend("project1.controller.View1", {
-            onInit: function () {
-
-            }
-        });
-    });
+      "use strict";
+  
+      return Controller.extend("project1.controller.View1", {
+        onInit: function () {
+          this.oRouter = this.getOwnerComponent().getRouter();
+        },
+        navToNextPage: function () {
+          this.oRouter.navTo("RouteList");
+        },
+      });
+    }
+  );
+  
